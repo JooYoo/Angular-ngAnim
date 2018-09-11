8396014fe06ec913a929956534cd4af993d2f1a6
JooYoo <jooyoo@outlook.com>
2018-09-11 17:05:58 +0200

    flip by Angular Animation Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { trigger, transition, useAnimation, state, style, animate } from '@angular/animations';
+import { flipInY, bounce } from 'ng-animate';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('popOverState',[
+
+      state('show',style({
+       transform:' rotateY(180deg)'
+      })),
+
+      state('hide',style({
+       transform: 'rotateY(0)'
+      })),
+
+      transition('show => hide', animate('300ms ease-out')),
+      transition('hide => show', animate('400ms ease-in'))
+      
+      // transition('*=>*',
+      // useAnimation(bounce))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'ngAnim';
+  
+  show:boolean =false;
+  
+  
+   get stateName() {
+    return this.show? 'show' : 'hide'
+  }
+  
+
+  animateMe(){
+    console.log("clicked")
+    this.show = !this.show
+  }
+
 }
