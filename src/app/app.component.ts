f7ba6750b6830e80eb05d822bf033fdf8a4141a9
JooYoo <jooyoo@outlook.com>
2018-09-13 16:02:12 +0200

    added swipe left and right

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { trigger, transition, useAnimation, state, style, animate } from '@angular/animations';
+import { trigger, transition, useAnimation, state, style, animate, keyframes } from '@angular/animations';
 import { flipInY, bounce } from 'ng-animate';
+import * as kf from './keyframes'
 
 @Component({
   selector: 'app-root',
@@ -20,18 +21,40 @@ import { flipInY, bounce } from 'ng-animate';
 
       transition('goFlip => goBack', animate('200ms ease-out')),
       transition('goBack => goFlip', animate('400ms ease-in'))
-    ]) 
+    ]),
+
+    trigger('cardAnimator',[
+    
+      transition('* => zoomOutDown', animate(1000, keyframes(kf.zoomOutDown))),
+
+      transition('* => wobble', animate(1000, keyframes(kf.wobble))),
+      transition('* => swing', animate(1000, keyframes(kf.swing))),
+      transition('* => jello', animate(1000, keyframes(kf.jello))),
+      transition('* => zoomOutRight', animate(1000, keyframes(kf.zoomOutRight))),
+      transition('* => zoomOutLeft', animate(1000, keyframes(kf.zoomOutLeft))),
+      transition('* => slideOutLeft', animate(1000, keyframes(kf.slideOutLeft))),
+      transition('* => rotateOutUpRight', animate(1000, keyframes(kf.rotateOutUpRight))),
+      transition('* => flipOutY', animate(1000, keyframes(kf.flipOutY))),
+    ])
   ]
 })
 export class AppComponent {
   
   title = 'ngAnim';
-
   isFlip: string = 'goBack';
- 
-  
   toggleFlip() {
     this.isFlip = (this.isFlip == 'goBack') ? 'goFlip' : 'goBack';
+  }
+
+  animationState: string
+  startAnimation(state){
+    console.log(state)
+    if(!this.animationState){
+      this.animationState = state
+    }
+  }
+  resetAnimationState(){
+    this.animationState = ''
   }
 
 }
